d25e3e4e82536008d801ade4c16b219326e68d3a
gysvalera228-ctrl <gysvalera228@gmail.com>
2026-09-20 01:13:51 +0300

    Create WindowPresetPopup.hpp

---
+++ src/UI/WindowPresetPopup.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include <Geode/Geode.hpp>
+
+class WindowPresetPopup : public geode::Popup {
+protected:
+    geode::TextInput* m_ifInput = nullptr;
+    geode::TextInput* m_winInput = nullptr;
+    geode::TextInput* m_textInput = nullptr;
+    cocos2d::CCSprite* m_colorSprite = nullptr;
+    cocos2d::ccColor4F m_currentColor = { 1.f, 1.f, 1.f, 1.f };
+
+    bool init();
+    void onLoad(cocos2d::CCObject*);
+    void onColorBtn(cocos2d::CCObject*);
+    void autoSave();
+    void onResetAll(cocos2d::CCObject*);
+    void onSwitchToFrames(cocos2d::CCObject*);
+
+public:
+    static WindowPresetPopup* create();
+    void showInstant();
+    void instantClose();
+};
